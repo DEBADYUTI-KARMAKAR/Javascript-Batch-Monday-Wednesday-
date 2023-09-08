dc669f09a331797a3c764f96f3886df5d5d7e063
Soumita Das <soumitad991@gmail.com>
2023-09-09 02:00:25 +0530

    Day 2

--- SoumitaCode/Swapping.js
+++ SoumitaCode/Swapping.js
@@ -4,12 +4,12 @@ let c;
 c=a;
 a=b;
 b=c;
-console.log(a);
-console.log(b);*/
+document.write(a+"<br>");
+document.write(b+"<br>");*/
 let a=4;
 let b=2;
 a=a+b;
 b=a-b;
 a=a-b;
-console.log(a);
-console.log(b);
+document.write(a+"<br>");
+document.write(b+"<br>");
---
+++ SoumitaCode/string.js
@@ -0,0 +1,22 @@
+let str="Hello JavaScript";
+document.write(str+"<br>");
+document.write(str.length +"<br>");
+document.write(str.charAt(2)+"<br>");
+document.write(str.indexOf('l')+"<br>");
+document.write(str.toLowerCase()+"<br>");
+document.write(str.toUpperCase()+"<br>");
+document.write(str.endsWith("pt")+"<br>");
+document.write(str.endsWith("l")+"<br>");
+document.write(str.includes("hii")+"<br>"); //Searching String
+document.write(str.includes("Hello")+"<br>");
+document.write(30+36+"Hello"+22+6+"<br>");
+document.write(10+"hii"+44+63+"hello"+"<br>");
+document.write(str.substring(2,8)+"<br>");
+document.write(str.split("")+"<br>");
+document.write(str.split(" ")+"<br>");
+document.write(str.split("a")+"<br>");
+document.write(str.split(" ")+"<br>");
+/*let st="  Hello  "
+document.write(st.length+"<br>");
+st=st.trim();
+document.write(st.length+"<br>");*/
