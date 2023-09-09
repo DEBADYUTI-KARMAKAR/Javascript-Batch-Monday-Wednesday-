6ec51cbbe8bd2ecd3582b97691bf141475ab5d9e
Soumita Das <soumitad991@gmail.com>
2023-09-09 10:18:18 +0530

    day2

--- SoumitaCode/fact.js
+++
@@ -1,7 +0,0 @@
-let num=5;
-let f=1;
-while(num>0){
-    f*=num;
-    num-=1;
-}
-document.write(f);
--- SoumitaCode/oddEven.js
+++
@@ -1,7 +0,0 @@
-let num=8;
-if(num%2==0){
-    document.write("Even");
-}
-else{
-    document.write("Odd");
-}
